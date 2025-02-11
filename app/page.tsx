aaaec08ce089496542dd5853ef6a46505c9dea83
Mathis <mathislegamin@gmail.com>
2025-02-11 17:14:21 +0100

    small modif

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+//app/page.tsx
+
 export const metadata = {
   title: "App Router",
 };
 
+import Counter from "app/counter";
+
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <>
+      <h1>App Router</h1>
+      <Counter />
+    </>
+    );
 }
